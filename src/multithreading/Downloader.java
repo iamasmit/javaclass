a4d13d5d92120822c91f22f6a10727b64c515c47
iamasmit <ashmitchaudhary82@gmail.com>
2024-02-23 12:47:20 +0545

    ten commit

--- src/multithreading/Downloader.java
+++ src/multithreading/Downloader.java
@@ -4,38 +4,44 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class Downloader implements Runnable {
     @Override
     public void run() {
-        // This thread runs a file or program from the internet
         BufferedInputStream bis = null;
         BufferedOutputStream bos = null;
         try {
-            URL url = new URL("");
+            URL url = new URL("https://www.rcsdk12.org/cms/lib/NY01001156/Centricity/Domain/4951/Head_First_Java_Second_Edition.pdf");
             URLConnection connection = url.openConnection();
-             bis = new BufferedInputStream(connection.getInputStream());
-             bos = new BufferedOutputStream(new FileOutputStream("D:\\emp.txt" +url.getFile()));
+            bis = new BufferedInputStream(connection.getInputStream());
+            bos = new BufferedOutputStream(new FileOutputStream("D:\\Head_First_Java_Second_Edition.pdf"));
+
             byte[] data = new byte[1024];
-            while (bis.read(data, 0, 1024) !=-1){
-                bos.write(data,0,1024);
-                int count =0;
-                System.out.println("Downloaded KB:" + ++count);
+            int bytesRead;
+            int totalBytesRead = 0;
+
+            while ((bytesRead = bis.read(data, 0, 1024)) != -1) {
+                bos.write(data, 0, bytesRead);
+                totalBytesRead += bytesRead;
+                System.out.println("Downloaded KB: " + totalBytesRead / 1024);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
-                if (bis!=null) bis.close();
-                if (bos!= null) bos.close();
-            }catch (IOException e) {
+                if (bis != null) bis.close();
+                if (bos != null) bos.close();
+            } catch (IOException e) {
                 throw new RuntimeException(e);
+            }
         }
+    }
 
-        }
-
+    public static void main(String[] args) {
+        Downloader downloader = new Downloader();
+        Thread t1 = new Thread(downloader);
+        t1.start();
     }
 }
